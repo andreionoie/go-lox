16cab467cb5b1b9fd41cbad22beec8e4bde9d453
Andrei Onoie <andrei.onoie@hotmail.com>
2024-10-13 22:52:02 +0300

    Add grouping expr visitor to interpret parentheses

--- cmd/myinterpreter/ast_interpreter.go
+++ cmd/myinterpreter/ast_interpreter.go
@@ -6,6 +6,10 @@ type AstInterpreter struct {
 	StubExprVisitor
 }
 
+func (itp *AstInterpreter) VisitGroupingExpr(e *GroupingExpr) (result interface{}, err error) {
+	return e.expr.Accept(itp)
+}
+
 func (itp *AstInterpreter) VisitLiteralExpr(e *LiteralExpr) (result interface{}, err error) {
 	if e.value == nil {
 		return "nil", nil
